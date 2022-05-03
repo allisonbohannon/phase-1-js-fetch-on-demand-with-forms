6ec36234305a9628b5c18af60f69a0c0b218dfe6
Allison Bohannon <akboh.github@gmail.com>
2022-05-02 19:55:12 -0700

    commit tree 8d466e92ee58364909f446bfbc852253d8d14a80

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
-}
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (e) => {
+        e.preventDefault(); 
+        const input = document.querySelector('input#searchByID');
+        fetchData(input); 
+    })
+}; 
+
+function fetchData(input) {
+    return fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(response => response.json())
+    .then(data => updateHTML(data));
+};
+
+function updateHTML(data) {
+    document.querySelector('h4').textContent = data.title; 
+    document.querySelector('p').textContent = data.summary; 
+}; 
+
 
 document.addEventListener('DOMContentLoaded', init);
+
